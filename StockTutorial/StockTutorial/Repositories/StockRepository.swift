4b27cd022358523354a6087fb7d1a84e4f489c10
민성홍 <sunghong32@naver.com>
2021-07-20 13:42:56 +0900

    get API Using Alpha Vantage

---
+++ StockTutorial/StockTutorial/Repositories/StockRepository.swift
@@ -0,0 +1,10 @@
+//
+//  StockRepository.swift
+//  StockTutorial
+//
+//  Created by 민성홍 on 2021/07/20.
+//
+
+protocol StockRepository {
+    var apiKey: String { get set }
+}
---
+++ StockTutorial/StockTutorial/Repositories/StockRepositoryImplement.swift
@@ -0,0 +1,10 @@
+//
+//  StockRepositoryImplement.swift
+//  StockTutorial
+//
+//  Created by 민성홍 on 2021/07/20.
+//
+
+class StockRepositoryImplement: StockRepository {
+    var apiKey: String = "H7EIQM981K7PGHAQ"
+}
