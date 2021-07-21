dd69267767e6a0d8243d530a454e3fcbfb13ec2a
민성홍 <sunghong32@naver.com>
2021-07-21 17:04:12 +0900

    make new error

--- StockTutorial/StockTutorial/Repositories/StockRepositoryImplement.swift
+++ StockTutorial/StockTutorial/Repositories/StockRepositoryImplement.swift
@@ -17,17 +17,62 @@ class StockRepositoryImplement: StockRepository {
     let decoder = JSONDecoder()
 
     func fetchStocksPublisher(keywords: String) -> AnyPublisher<StockResult, Error> {
-        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
-            let error: StockRepositoryError = .encoding
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+
+        switch queryResult {
+        case .failure(let error):
             return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
         }
-        let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(apiKey)"
-        guard let url = URL(string: urlString) else {
+
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(query)&apikey=\(apiKey)")
+
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        }
+    }
+
+    func fetchTimeSeriesPublisher(keywords: String) -> AnyPublisher<TimeSeriesMonthlyAdjusted, Error> {
+        let queryResult = parseQueryString(text: keywords)
+        var query = ""
+
+        switch queryResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let value):
+            query = value
+        }
+
+        let urlResult = parseURL(urlString: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=\(query)&apikey=\(apiKey)")
+
+        switch urlResult {
+        case .failure(let error):
+            return Fail(error: error).eraseToAnyPublisher()
+        case .success(let url):
+            return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: TimeSeriesMonthlyAdjusted.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+        }
+    }
+
+    private func parseURL(urlString: String) -> Result<URL, Error> {
+        if let url = URL(string: urlString) {
+            return .success(url)
+        } else {
             let error: StockRepositoryError = .bad_url
-
-            return Fail(error: error).eraseToAnyPublisher()
+            return .failure(error)
         }
+    }
 
-        return URLSession.shared.dataTaskPublisher(for: url).map { $0.data }.decode(type: StockResult.self, decoder: decoder).receive(on: RunLoop.main).eraseToAnyPublisher()
+    private func parseQueryString(text: String) -> Result<String, Error> {
+        if let query = text.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            return .success(query)
+        } else {
+            let error: StockRepositoryError = .encoding
+            return .failure(error)
+        }
     }
 }
